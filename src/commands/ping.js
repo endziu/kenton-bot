7e8a2e3a19d2fd505480999e1befb5459a018197
endziu <koper.andrzej@gmail.com>
2021-03-01 16:39:30 +0100

    simplify ping :)

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,4 +1,1 @@
-module.exports = async (message, args) => {
-  const timeTaken = message.createdTimestamp - Date.now()
-  message.channel.send(`${JSON.stringify(args)} => pong! :: latency: ${timeTaken}ms.`)
-}
+module.exports = async (message) => message.channel.send(`pong!`)
